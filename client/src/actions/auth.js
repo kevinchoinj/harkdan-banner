fe693c4dbe3b2ea611374987bc2d7bdde2a4fe81
kevinchoinj <kevinchoinj@yahoo.com>
2020-11-03 13:55:12 -0600

    added check login status failure case for when they are not logged in :)

--- client/src/actions/auth.js
+++ client/src/actions/auth.js
@@ -1,10 +1,8 @@
 import Cookies from 'js-cookie';
 
-export const LOGIN_PASSPORT_STARTED = Symbol('LOGIN_PASSPORT_STARTED');
 export const LOGIN_PASSPORT_SUCCEEDED = Symbol('LOGIN_PASSPORT_SUCCEEDED');
 export const LOGIN_PASSPORT_FAILURE = Symbol('LOGIN_PASSPORT_FAILURE');
 
-export const FIND_PASSPORT_STARTED = Symbol('FIND_PASSPORT_STARTED');
 export const FIND_PASSPORT_SUCCEEDED = Symbol('FIND_PASSPORT_SUCCEEDED');
 export const FIND_PASSPORT_FAILURE = Symbol('FIND_PASSPORT_FAILURE');
 
@@ -12,13 +10,16 @@ export const LOG_OUT = Symbol('LOG_OUT');
 
 const loginPassportSucceeded = data => ({type: LOGIN_PASSPORT_SUCCEEDED, data});
 const findPassportSucceeded = data => ({type: FIND_PASSPORT_SUCCEEDED, data});
+const findPassportFailure = data => ({type: FIND_PASSPORT_FAILURE, data});
 
 export const withRequest = ({request}) => request;
 
 export function findPassport() {
   const accessString = Cookies.get('JWT');
-  console.log(accessString);
-  return findPassportSucceeded(accessString);
+  if (accessString) {
+    return findPassportSucceeded(accessString);
+  }
+  return findPassportFailure();
 }
 
 export function loginPassport(values) {
